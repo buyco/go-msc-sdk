0abed97c53a1440375f3faf4f7b8fae79f5c079f
Alexandre Del Vecchio <alexandre@buyco.co>
2021-09-14 18:03:11 +0200

    WIP

--- msc/client/auth.go
+++ msc/client/auth.go
@@ -1,9 +1,11 @@
-package msc
+package client
 
 import (
 	"crypto/x509"
 	"encoding/base64"
+	"encoding/hex"
 	"encoding/pem"
+	"math/rand"
 	"strconv"
 	"strings"
 	"time"
@@ -14,7 +16,14 @@ import (
 	http "github.com/imroc/req"
 )
 
-type AuthClient struct {
+const (
+	//authTokenPath = "/oauth2/token"
+	authTokenPath = "/oauth2/v2.0/token"
+	clientAssertionType = "urn:ietf:params:oauth:client-assertion-type:jwt-bearer"
+	grantType = "client_credentials"
+)
+
+type Auth struct {
 	Url             string
 	CertFingerprint string
 	PrivateKey      string
@@ -36,6 +45,14 @@ func x5t(fingerprint string) string {
 	return s
 }
 
+func randomHex(n int) (string, error) {
+	bytes := make([]byte, n)
+	if _, err := rand.Read(bytes); err != nil {
+		return "", err
+	}
+	return hex.EncodeToString(bytes), nil
+}
+
 type CustomClaims struct {
 	Audience  string `json:"aud,omitempty"`
 	ExpiresAt int64  `json:"exp,omitempty"`
@@ -50,27 +67,30 @@ func (c CustomClaims) Valid() error {
 	return nil
 }
 
-func (c *AuthClient) Token() (string, time.Duration, error) {
-
-	block, _ := pem.Decode([]byte(c.PrivateKey))
-
+func (a *Auth) Token(expiresAt int64) (string, time.Duration, error) {
+	block, _ := pem.Decode([]byte(a.PrivateKey))
 	key, err := x509.ParsePKCS8PrivateKey(block.Bytes)
+	if err != nil {
+		return "", 0, err
+	}
+
+	randID, err := randomHex(12)
 	if err != nil {
 		return "", 0, err
 	}
 
 	now := time.Now().Unix()
 	claims := CustomClaims{
-		Subject:   c.ClientId,
-		Issuer:    c.ClientId,
-		Id:        "myid",
+		Subject:   a.ClientId,
+		Issuer:    a.ClientId,
+		Id:        randID,
 		NotBefore: now,
-		Audience:  c.Url + "/" + c.TenantId + "/oauth2/token",
-		ExpiresAt: now + 1200,
+		Audience:  a.Url + "/" + a.TenantId + authTokenPath,
+		ExpiresAt: now + expiresAt,
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodRS256, claims)
-	token.Header["x5t"] = x5t(c.CertFingerprint)
+	token.Header["x5t"] = x5t(a.CertFingerprint)
 
 	signed, err := token.SignedString(key)
 
@@ -78,19 +98,19 @@ func (c *AuthClient) Token() (string, time.Duration, error) {
 		return "", 0, err
 	}
 	form := http.Param{
-		"tenant":                c.TenantId,
-		"client_id":             c.ClientId,
-		"scope":                 c.AppId + "/.default",
-		"client_assertion_type": "urn:ietf:params:oauth:client-assertion-type:jwt-bearer",
+		"tenant":                a.TenantId,
+		"client_id":             a.ClientId,
+		"scope":                 a.AppId + "/.default",
+		"client_assertion_type": clientAssertionType,
 		"client_assertion":      signed,
-		"grant_type":            "client_credentials",
+		"grant_type":            grantType,
 	}
 
 	headers := http.Header{
 		"Accept":       "application/json",
 		"Content-Type": "application/x-www-form-urlencoded",
 	}
-	r, err := http.Post(c.Url+"/"+c.TenantId+"/oauth2/v2.0/token", headers, form)
+	r, err := http.Post(a.Url + "/" + a.TenantId + authTokenPath, headers, form)
 	if err != nil {
 		return "", 0, nil
 	}
@@ -98,7 +118,7 @@ func (c *AuthClient) Token() (string, time.Duration, error) {
 
 	err = r.ToJSON(&d)
 
-	expires_in, err := time.ParseDuration(d["expires_in"])
-	return d["access_token"], expires_in, nil
+	expiresIn, err := time.ParseDuration(d["expires_in"])
+	return d["access_token"], expiresIn, nil
 
 }
